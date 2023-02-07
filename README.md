03a8cb1f83acc7c88ebb4f8e78517936c63a68c4
Apesteguia <ascipendcip@gmail.com>
2023-02-07 21:19:06 +0100

    Embed Message

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Running Java jar with a Bat file:
+
+java -jar discordBot.jar
--- src/main/java/apesteguia/dani/discordbot/Main.java
+++ src/main/java/apesteguia/dani/discordbot/Main.java
@@ -2,13 +2,21 @@ package apesteguia.dani.discordbot;
 
 import apesteguia.dani.discordbot.configuration.ConfigFile;
 import apesteguia.dani.discordbot.configuration.ConfigValues;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.guild.update.GuildUpdateNameEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main extends ListenerAdapter {
 
@@ -25,6 +33,7 @@ public class Main extends ListenerAdapter {
         jdaBuilder.setStatus(OnlineStatus.ONLINE);
         jdaBuilder.setActivity(Activity.competing(ConfigValues.BOT_STATUS));
         jdaBuilder.addEventListeners(new Main());
+        jdaBuilder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
         jdaBuilder.build();
     }
 
@@ -46,15 +55,43 @@ public class Main extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        String[] arguments = event.getMessage().getContentRaw().split(" ");
-        if(arguments[0].equals("!mention")){
+        //String[] arguments = event.getMessage().getContentRaw().split(" ");
+        //if(arguments[0].equals("!mention")){
+        List<String> words = Arrays.asList(event.getMessage().getContentRaw().split("\\s+"));
+        if(words.contains("!mention")){
             Member member = event.getMessage().getMentions().getMembers().get(0);
             if(!member.getUser().isBot()){
                 event.getChannel().sendMessage("Have a good day "+member.getUser().getAsMention()).queue();
             }else{
                 event.getChannel().sendMessage("Sorry, I can't mention bots").queue();
             }
-            //event.getChannel().sendMessage("How are you doing today").queue();
+
+        }
+        else if(words.contains("!sendembed")){
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.setTitle("Embed message");
+            embedBuilder.setColor(Color.MAGENTA);
+            embedBuilder.setDescription("Embed description testing" +
+                    "\n This is a 2nd line" +
+                    "\n This is a 3rd line");
+            embedBuilder.setFooter("Embed footer testing", event.getAuthor().getAvatarUrl());
+            embedBuilder.setThumbnail(event.getAuthor().getAvatarUrl());
+            embedBuilder.setImage(event.getAuthor().getAvatarUrl());
+            event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
+
+
+
+        }
+    }
+
+
+
+    @Override
+    public void onGuildUpdateName(@NotNull GuildUpdateNameEvent event) {
+        TextChannel textChannel = event.getGuild().getSystemChannel();
+        if(textChannel != null){
+            textChannel.sendMessage("**Discord name was updated to: "+ event.getNewName()+"**").queue();
+
         }
     }
 }
